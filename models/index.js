7e35277b70284ff77b61761d7134cf03286c8234
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-05 14:45:35 -0600

    added post routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,14 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 
 
 
