6da32a6b07b21b8506d250540be2a3a58dbb98e1
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-05 11:21:14 -0600

    working on user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Create a User model
+class User extends Model {}
+
+User.init({
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "user",
+});
+
+module.exports = User;
