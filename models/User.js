0f2aa827a6ab5c9e26cd137b5dcf123db5a1f6e6
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-05 11:42:15 -0600

    completed user model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,39 @@ const sequelize = require("../config/connection");
 class User extends Model {}
 
 User.init({
+    
+     id: {
+        DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true   
+     },  
+
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        uniquie: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+},  
+    
+    
+    
+    {
   sequelize,
   timestamps: false,
   freezeTableName: true,
--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+
+
+
+
+
+
+
+module.exports = { User };
