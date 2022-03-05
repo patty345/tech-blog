6c58a69d5127ab54a00883ec8dff1a783f2b2ab2
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-05 14:58:55 -0600

    added the associations indexjs file

--- models/index.js
+++ models/index.js
@@ -10,10 +10,24 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 
 
 
 
-
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
