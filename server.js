b22763e17c3880428984d8170e5fc57695ec93c8
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-05 19:34:15 -0600

    added home-routes and handlebars

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
+
+
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
 
 
 const app = express();
@@ -12,8 +18,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Handlebars default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
